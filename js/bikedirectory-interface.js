1e19b39c86501dae84701ae504ef792a00882ac2
Asia Kane and Kai de Rochemont <asia@>
2017-05-24 16:56:38 -0700

    add function to create list of stolen bikes

--- js/bikedirectory-interface.js
+++ js/bikedirectory-interface.js
@@ -1,10 +1,16 @@
 var Bike = require('./../js/bikedirectory.js').bikeModule;
 
+function displayBikes(bikes){
+  bikes.forEach(function(bike){
+    $('.stolen-output').append(`<li>${bike}</li>`);
+  });
+}
+
 $(document).ready(function(){
   $('#generate-bikes').click(function(){
     var zip = $('#zip').val();
     var distance = parseInt($('#distance').val());
     var newSearch = new Bike();
-    newSearch.listByProximity(zip, distance);
+    newSearch.listByProximity(zip, distance, displayBikes);
   });
 });
--- js/bikedirectory.js
+++ js/bikedirectory.js
@@ -1,11 +1,14 @@
 function Bike() {
-
 }
 
-Bike.prototype.listByProximity = function (zip, distance) {
+Bike.prototype.listByProximity = function (zip, distance, displayBikes) {
   $.get(`https://bikeindex.org/api/v3/search?page=1&per_page=25&location=${zip}&distance=${distance}&stolenness=proximity`)
     .then(function(response) {
-      console.log(response);
+      var bikes = [];
+      response.bikes.forEach(function(bike) {
+        bikes.push(bike.frame_model);
+      });
+      displayBikes(bikes);
     })
     .fail(function(error) {
       $('.output').text(error.responseJSON.message);
