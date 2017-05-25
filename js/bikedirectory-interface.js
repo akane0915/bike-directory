a7ce2cb3e224c378aedf2c86990ae6f6baf9eb28
Asia Kane and Kai de Rochemont <asia@>
2017-05-25 14:47:20 -0700

    add function to gather coordinates

--- js/bikedirectory-interface.js
+++ js/bikedirectory-interface.js
@@ -1,6 +1,6 @@
 var Bike = require('./../js/bikedirectory.js').bikeModule;
 
-function displayBikes(bikes){
+function displayBikes(bikes, displayCoordinates){
   bikes.forEach(function(bike){
     if (bike.thumb === null){
       bike.thumb = "http://www.toysrus.com/graphics/product_images/pTRU1-3049321enh-z6.jpg";
@@ -12,10 +12,20 @@ function displayBikes(bikes){
         <li>Year: ${bike.year}</li>
         <li>Serial Number: ${bike.serial}</li>
         <li><img class="bike-img" src="${bike.thumb}"></li>
+        <li class="hidden">${bike.id}</li>
         </ul></li>`);
   });
-  $('.show-on-click').click(function() {
+  $('.show-on-click').click(function(displayCoordinates) {
     $(this).children().toggle();
+    var bike = $(this).children().children()[5];
+    var bikeID = $(bike).text();
+    var newCoordinates = new Bike();
+    var bikeDiv = this;
+    function displayCoordinates(location, bikeDiv) {
+      // console.log(location);
+      $(bikeDiv).append(`<h1>${location.latitude}</h1>`)
+    }
+    var location = newCoordinates.getCoordinates(bikeID, displayCoordinates, bikeDiv);
   });
 }
 
--- js/bikedirectory.js
+++ js/bikedirectory.js
@@ -1,6 +1,23 @@
 function Bike() {
 }
 
+Bike.prototype.getCoordinates = function(id, displayCoordinates, bikeDiv) {
+  $.get(`https://bikeindex.org:443/api/v3/bikes/${id}`)
+    .then(function(response) {
+      var latitude = response.bike.stolen_record.latitude;
+      var longitude = response.bike.stolen_record.longitude;
+      function Location(latitude, longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+      }
+      var newLocation = new Location(latitude, longitude);
+      displayCoordinates(newLocation, bikeDiv);
+      })
+    .fail(function(error) {
+      $('.output').text(error.responseJSON.message);
+    });
+};
+
 Bike.prototype.listByProximity = function (zip, distance, displayBikes) {
   $.get(`https://bikeindex.org/api/v3/search?page=1&per_page=25&location=${zip}&distance=${distance}&stolenness=proximity`)
     .then(function(response) {
